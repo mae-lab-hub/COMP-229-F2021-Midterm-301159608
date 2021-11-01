acda4348f6bd597127487a8559dcd9795487cb55
@mae-lab-hub <asmaeallou@outlook.com>
2021-11-01 17:32:03 -0400

    Added a header.

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,10 @@
+/* 
+File name      : db.js
+Student’s Name : Asmae Allou  
+StudentID      : 301159608
+Date           : 1-11-2021
+*/
+
 module.exports = {
   //local MongoDB deployment ->
   "URI": "mongodb+srv://Mae:ixBX0tNouQ3xZaOf@cluster0.c5qju.mongodb.net/midterm?retryWrites=true&w=majority"
